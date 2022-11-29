987778715cdc7ac14ca474ef9d50aa634c9260cd
Omar <cooool159357@gmail.com>
2022-11-29 23:13:52 +0200

    Initial Islami App & Home Screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -9,6 +9,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar:AppBar(
+        backgroundColor: Colors.transparent,
         elevation: 0.0,
         centerTitle: true,
         title: Text('Islami'),
